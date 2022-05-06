d53ff3e7444a54fb0d6022858b7d46b336d890eb
Gabriel <gabrieldiascatarin26@gmail.com>
2022-05-06 17:01:38 -0300

    BLOG-4 Favoritar card

--- js/script.js
+++ js/script.js
@@ -11,3 +11,35 @@ const filterCard = () => {
         };
     };
 };
+
+
+const handleFavorite = () => {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    favorites.forEach(function(favorite){
+        document.getElementById(favorite).classList.add('fav');
+        document.getElementById(favorite).parentNode.parentNode.setAttribute('data-tooltip', 'Desfavoritar')
+    });
+
+    document.querySelectorAll('.heart-svg').forEach(itens => {
+        itens.addEventListener('click', function(e){
+            const id = e.target.id,
+                item = e.target,
+                index = favorites.indexOf(id);
+            if (!id) return;
+            if (index == -1) {
+                favorites.push(id);
+                item.classList.add('fav');
+                item.parentNode.parentNode.setAttribute('data-tooltip', 'Desfavoritar')
+                console.log(item.parentNode.parentNode);
+            } else {
+                favorites.splice(index, 1);
+                item.classList.remove('fav');
+                item.parentNode.parentNode.setAttribute('data-tooltip', 'Favoritar')
+            };
+            localStorage.setItem('favorites', JSON.stringify(favorites));
+        });
+    });
+};
+
+handleFavorite();
